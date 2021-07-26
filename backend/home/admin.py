c70c4dee18b421c97ecf4c5786462692c2fc1e14
Crowdbotics <team@crowdbotics.com>
2021-07-26 09:51:01 +0000

    Update models

--- backend/home/admin.py
+++ backend/home/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Platforms
+
+admin.site.register(Platforms)
 
 # Register your models here.
--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,9 @@
+from django.conf import settings
 from django.db import models
+
+
+class Platforms(models.Model):
+    "Generated Model"
+    name = models.TextField()
+    site = models.URLField()
+    logo = models.URLField()
